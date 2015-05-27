6b0d65ef72450fb19dc3c101b30e8c26142b0652
David Lee <itxawjteeb@gmail.com>
2015-05-26 23:48:49 -0700

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by David Lee on 5/26/15.
+//  Copyright (c) 2015 David Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
